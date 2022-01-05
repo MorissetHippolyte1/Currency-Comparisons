278437c351a2c0ff3fd5cd463227a68c618b18eb
Charlie Money <cmoney5748@gmail.com>
2022-01-05 13:00:34 -0500

    fixed an issue with the html linking incorectly to the script.js

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function apitest (){
     
 }
 apitest;
+
+function currencylist (){
+    var requestUrl = 'https://api.coingecko.com/api/v3/coins/markets';
+
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+    
+}
