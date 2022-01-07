bd2eadeaf1972b4c5e6b7f2112f2e40ed6657a73
Nick Lyle <nicklyle2009@gmail.com>
2022-01-07 11:07:31 -0500

    added dropdown option, doesnt display, likely because displays quotations

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+var titleEl = document.querySelector('.title')
+var searchFormEl = document.querySelector('#search-form')
+var amountInputEl = document.querySelector('#amount-input')
+var formatInputEl = document.querySelector('#format-input')
+var compareEl = document.querySelector('.compare')
+var addSavedEl = document.querySelector('.addSaved')
+var boxHeader = document.querySelector('boxHeader')
+var boxTitle = document.querySelector('boxTitle')
+var boxInfoEl = document.querySelector('.boxInfo')
+var currencyBoxEl = document.querySelector('.currencyBoxEl')
+var currencyCompareEl = document.querySelector('.currencyCompare')
+var newsEl = document.querySelector('.news')
+var articleEl = document.querySelector('.article')
+var cryptoPrice = document.getElementById("crypto");
+var govPrice = document.getElementById("gov");
+
 function currencylistusd (){
     var requestUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd';
 
@@ -13,7 +29,7 @@ function currencylistusd (){
 }
 currencylistusd();
 
-function matchcrypto(currency) {
+function matchcrypto(currency, amount) {
     var requestUrl = new URL('https://api.coingecko.com/api/v3/coins/markets?');
     x= "vs_currency"
     y= currency
@@ -25,10 +41,15 @@ function matchcrypto(currency) {
       .then(function (data) {
         console.log("function input currency");
         console.log(data);
+        for ( var i = 0; i<9;i++){
+          cryptoPrice.appendChild(document.createElement('ul')).textContent = 
+          parseFloat(amount/data[i].current_price).toFixed(2) + " " + data[i].name;
+          formatInputEl.appendChild(document.createElement('option')).value = "Crypto "  + data[i].name;
+        }
       });
     
 }
-matchcrypto("czk");
+matchcrypto("usd", 1000000);
 
 function logtop50cryptoprices(currency) {
     var requestUrl = new URL('https://api.coingecko.com/api/v3/coins/markets?');
@@ -78,30 +99,74 @@ function govcurrencyexchange (currency, amount){
     .then(function (data) {
       console.log("exchange")
       console.log(data);
+      console.log("data value")
+      var govArrayPrice = Object.values(data.rates);
+      var govArrayKey = Object.keys(data.rates);
+      console.log(govArrayPrice);
+      console.log(govArrayKey);
+      for ( var i = 0; i<9;i++){
+        govPrice.appendChild(document.createElement('ul')).textContent = 
+        parseFloat(govArrayPrice[i]).toFixed(2) + " " + govArrayKey[i];
+        for ( var i = 0; i<19; i++){ 
+        formatInputEl.appendChild(document.createElement('option')).value = "Gov "  + govArrayKey[i];
+      }
+    }
     });
   
 }
 govcurrencyexchange("USD", 10);
+
+function news (){
+  fetch("https://crypto-news-live.p.rapidapi.com/news/coindesk", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "crypto-news-live.p.rapidapi.com",
+      "x-rapidapi-key": "1c706ef656msh2980b7876759cc5p1013b9jsn6d3391c16b20"
+    }
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(response => {
+    console.log("news")
+    console.log(response);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
+news();
 
 var amountFormEl = document.querySelector('#currency-form');
 
 function handleSearchFormSubmit(event) {
   event.preventDefault();
 
-  var searchInputVal = document.querySelector('#amount-input').value;
+  var amountInputVal = document.querySelector('#amount-input').value;
   var formatInputVal = document.querySelector('#format-input').value;
 
-  if (!searchInputVal) {
+  if (!amountInputVal) {
     console.error('You need a search input value!');
     return;
   }
 
-  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+  var queryString = 'https://api.coingecko.com/api/v3/coins/list' + formatInputVal;
 
-  location.assign(queryString);
+  (queryString);
 }
 
+// function addFormSelectors() { 
+//   var dropDownChoice = function() {
+//    var dropDownEl = document.createElement('option');
+//    var dropDownChoice =  
+//     document.appendChild
+//   document.createElement()
+  //for loop for creating element
+  //need to pull information from api
+// document.createElement(variable ), document.body.appendChild(tag)
+// add event listener .addEventListener("click", function() 
 
 
+//}
 // amountFormEl.addEventListener('submit', handleSearchFormSubmit);
 
